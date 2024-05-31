056c8a9d735c4ba00344d450b3d916f6deadd740
xp660 <36498576+xp660@users.noreply.github.com>
2024-05-31 08:58:21 +0800

    CODE

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#collapse").on("click",function(){
+        $("#sidebar").toggleClass("active");
+        $(".fa-align-left").toggleClass("fa-chevron-circle-right");
+       
+        
+    })
+})
